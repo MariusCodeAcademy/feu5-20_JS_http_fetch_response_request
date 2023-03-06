b0d2e52b38741f14b807f3bbb563ef78925dd145
Marius CA <marius@example.com>
2023-03-06 16:51:07 +0200

    json

---
+++ js/json.js
@@ -0,0 +1,25 @@
+'use strict';
+console.log('json.js file was loaded');
+
+const user1 = {
+  username: 'James',
+  town: 'London',
+  age: 45,
+  isAwesome: true,
+  movies: ['007', 'Return of Bond'],
+};
+
+console.log('user1 ===', user1);
+
+// konvertuoti user1 i Json formata
+const user1Json = JSON.stringify(user1);
+console.log('user1Json ===', user1Json);
+console.log('user1Json typeof ===', typeof user1Json);
+
+// konvertuoti i javascript objekta ar masyva
+const user1After = JSON.parse(user1Json);
+console.log('user1After ===', user1After);
+
+console.log(Boolean(user1 === user1After));
+console.log(Boolean({ a: 1 } === { a: 1 }));
+console.log(Boolean(JSON.stringify({ a: 1 }) === JSON.stringify({ a: 1 })));
